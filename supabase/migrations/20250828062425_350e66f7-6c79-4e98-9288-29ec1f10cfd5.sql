b3cd4e50d7296cdd4e587c4855b70d7010b0c998
developerAS123 <ankitsinghrajput8521@gmail.com>
2025-08-30 18:19:14 +0530

    Updated 20250828062425_350e66f7-6c79-4e98-9288-29ec1f10cfd5.sql

--- supabase/migrations/20250828062425_350e66f7-6c79-4e98-9288-29ec1f10cfd5.sql
+++ supabase/migrations/20250828062425_350e66f7-6c79-4e98-9288-29ec1f10cfd5.sql
@@ -178,7 +178,7 @@ CREATE POLICY "Manufacturers can update application status" ON public.job_applic
 CREATE OR REPLACE FUNCTION public.handle_new_user()
 RETURNS TRIGGER
 LANGUAGE plpgsql
-SECURITY DEFINER SET search_path = public
+SECURITY INVOKER SET search_path = public
 AS $$
 BEGIN
   INSERT INTO public.profiles (user_id, user_type, name, phone, location)
